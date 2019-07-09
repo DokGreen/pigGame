b81801cbd3e74832e2af18e359ee1d5b4cafd44a
Reece <rfarnworth92@gmail.com>
2019-07-09 18:35:26 +0100

    added img change on click

--- resources/pig.js
+++ resources/pig.js
@@ -7,11 +7,18 @@ activePlayer = 0;
 document.querySelector('.dice').style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    //bring the dice back
-    document.querySelector('.dice').style.display = 'block';
-    
     //add the value of dice to current score
     dice = Math.floor(Math.random() * 6) + 1;
     document.querySelector('#current-' + activePlayer).textContent += dice;
+    
+    //bring the dice back
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    
+    //change dice img to reflect dice var
+    diceDOM.src = 'resources/media/dice-' + dice + '.png';
+   
+    
+    
 });
 
