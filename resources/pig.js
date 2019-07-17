fc76898684bb79c778859e641c3433ae11dddfb3
Reece <rfarnworth92@gmail.com>
2019-07-17 19:36:38 +0100

    complete hold function and score update

--- resources/pig.js
+++ resources/pig.js
@@ -30,15 +30,42 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.getElementById('current-' + activePlayer).textContent = roundScore;
         
         //else reset score to 0 and change the player
-    } else {
-        roundScore = 0;
-        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {  
         //if the player is 0 make it 1, if its anything else rewind to 0
         activePlayer === 0? activePlayer = 1: activePlayer = 0;
+        //reset the scores to 0
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        //change the active styles
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        
+        //final step to reset the scoreboard
+        document.querySelector('.dice').style.display = 'none';
     }
-   
-    
-    
     
 });
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //on click add the roundScore to score
+    scores[activePlayer] += roundScore;
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    
+    //reset the board
+    document.querySelector('.dice').style.display = 'none';
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    //swap to the other player
+    activePlayer === 0? activePlayer = 1: activePlayer = 0;
+
+    
+    //change the active styles
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+})
 
