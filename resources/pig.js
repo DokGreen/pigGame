3098be8d7645a29cae695668e2888dcf4aa0c70c
Reece <rfarnworth92@gmail.com>
2019-07-17 17:50:28 +0100

    created the final vars

--- resources/pig.js
+++ resources/pig.js
@@ -4,7 +4,14 @@ roundScore = 0;
 activePlayer = 0;
 //dice = Math.floor(Math.random() * 6) + 1;
 
+
+//set the board
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     //add the value of dice to current score
@@ -18,6 +25,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     //change dice img to reflect dice var
     diceDOM.src = 'resources/media/dice-' + dice + '.png';
    
+    
     
     
 });
